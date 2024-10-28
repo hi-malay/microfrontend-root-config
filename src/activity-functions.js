8005adb3f5408e0fc43bf35e1980adaba7572c38
Malay <hi.malay879@gmail.com>
2024-10-28 18:03:17 +0530

    FIXED issue related to environment and path

--- src/activity-functions.js
+++ src/activity-functions.js
@@ -1,12 +1,16 @@
+// Determine base path dynamically
+const isLocal = window.location.hostname === "localhost";
+const basePath = isLocal ? "" : "/mf-root-config";
+
 export function navbar(location) {
   return true; // The navbar is always active
 }
 
 export function employees(location) {
-  return location.pathname === "/employees";
+  return location.pathname === `${basePath}/employees`;
 }
 
 export function employeeDetails(location) {
-  const regex = /^\/employees\/\d+?$/;
-  return location.pathname.match(regex);
+  const regex = new RegExp(`^${basePath}/employees/\\d+$`);
+  return regex.test(location.pathname);
 }
